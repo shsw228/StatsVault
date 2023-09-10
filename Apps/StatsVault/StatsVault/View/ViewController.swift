1d79aff8ada72d6e35b7bf46fa060806ec64f2c0
Tkng <13506491+shsw228@users.noreply.github.com>
2023-09-10 19:13:18 +0900

    add 画面のタイトルなどの初期設定を追加

--- Apps/StatsVault/StatsVault/View/ViewController.swift
+++ Apps/StatsVault/StatsVault/View/ViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var collectionView:UICollectionView! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        configureCollectionView()
     }
 
 
 }
 
+// MARK: タイトルなどを設定
+extension ViewController {
+    private func setupView() {
+        setBackgroundColor()
+        setTitleView()
+        setSettingButton()
+    }
+
+    private func setTitleView() {
+        // TODO: リソース切り出し
+        // タイトル設定
+        navigationItem.title = "ゲーム一覧"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+
+    }
+    private func setSettingButton() {
+        // 右にアイコン表示
+        // TODO: アプリ設定画面実装
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain, target: nil, action: nil)
+    }
+    private func setBackgroundColor() {
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+
+// MARK: CollectionView関連
+extension ViewController {
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+
+    }
+}
